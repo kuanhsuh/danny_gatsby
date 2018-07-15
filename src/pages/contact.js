f1992760a980ca8a1a0de7e9bb2abf30742b2ecb
dannyhuang <kuanhsuh@gmail.com>
2018-07-15 21:45:42 +0800

    Finish contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,18 +10,29 @@ const Contact = () => (
         Got a question? Just drop me a message. I will respond as soon as
         possible
       </h4>
-      <form className="mt20" name="dannywebdev-contact" method="POST" netlify>
+      <form
+        className="mt20"
+        name="dannywebdev-contact"
+        method="POST"
+        netlify-honeypot="bot-field"
+        netlify
+      >
+        <p style={{ display: "none" }}>
+          <label>
+            Don’t fill this out if you're human: <input name="bot-field" />
+          </label>
+        </p>
         <div className="form-group">
           <label htmlFor="name">Name</label>
-          <input type="text" className="form-control" id="name" />
+          <input type="text" className="form-control" id="name" required />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email</label>
-          <input type="email" className="form-control" id="email" />
+          <input type="email" className="form-control" id="email" required />
         </div>
         <div className="form-group">
           <label htmlFor="message">Message</label>
-          <textarea className="form-control" id="message" rows="10" />
+          <textarea className="form-control" id="message" rows="10" required />
         </div>
         <div data-netlify-recaptcha />
         <button className="btn-outline btn-md">SUBMIT</button>
