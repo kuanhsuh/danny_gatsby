a7c01e298d1c1552dc98696c86757c1fdb891033
dannyhuang <kuanhsuh@gmail.com>
2018-07-15 21:28:27 +0800

    React Helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Default Starter"
+    title: "Danny Huang | Web Developer"
   },
   plugins: [
     "gatsby-plugin-react-helmet",
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -11,8 +11,11 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: "description", content: "Sample" },
-        { name: "keywords", content: "sample, something" }
+        {
+          name: "description",
+          content: "Frontend Web Developer | Danny Huang"
+        },
+        { name: "keywords", content: "React, Vue, Javascript, CSS" }
       ]}
     />
     <Nav />
