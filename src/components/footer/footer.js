c2cf57a5a5bacd413b0539a785f38b1298cf8c65
dannyhuang <kuanhsuh@gmail.com>
2018-07-15 09:57:03 +0800

    fix Sprite and mobile font size

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,30 +1,30 @@
 import React from "react";
 import "./footer.scss";
-// import sprite from "./sprite.svg";
+import sprite from "assets/sprite.svg";
 
 const Footer = props => (
   <footer>
     <div className="icons">
       <a href="http://github.com/kuanhsuh">
         <svg className="icon">
-          {/* <use xlinkHref={`${sprite}#icon-github`} /> */}
-          <use xlinkHref="#icon-github" />
+          <use xlinkHref={`${sprite}#icon-github`} />
+          {/* <use xlinkHref="#icon-github" /> */}
         </svg>
       </a>
       <a href="https://www.facebook.com/dannyhuang24">
         <svg className="icon">
-          <use xlinkHref="#icon-facebook" />
-          {/* <use xlinkHref={`${sprite}#icon-facebook`} /> */}
+          {/* <use xlinkHref="#icon-facebook" /> */}
+          <use xlinkHref={`${sprite}#icon-facebook`} />
         </svg>
       </a>
       <a href="https://www.youtube.com/user/kuanhsuh">
         <svg className="icon">
-          <use xlinkHref="#icon-youtube" />
-          {/* <use xlinkHref={`${sprite}#icon-youtube`} /> */}
+          {/* <use xlinkHref="#icon-youtube" /> */}
+          <use xlinkHref={`${sprite}#icon-youtube`} />
         </svg>
       </a>
     </div>
-    <p>Copyright © 2018 Danny Huang</p>
+    <p>Copyright © 2018 Danny Huang demo</p>
   </footer>
 );
 
