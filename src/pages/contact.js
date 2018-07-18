8229c507a535e36c1ff22f32908616c09e5e7813
dannyhuang <kuanhsuh@gmail.com>
2018-07-18 14:32:49 +0800

    fixed

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,49 +10,8 @@ const Contact = () => (
         Got a question? Just drop me a message. I will respond as soon as
         possible
       </h4>
-      {/* <form
-        className="mt20"
-        name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="bot-field" />
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            name="name"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">Message</label>
-          <textarea
-            className="form-control"
-            id="message"
-            rows="10"
-            name="message"
-            required
-          />
-        </div>
-        <button className="btn-outline btn-md" type="submit">
-          SUBMIT1
-        </button>
-      </form> */}
       <form
+        className="mt20"
         name="contact"
         method="post"
         data-netlify="true"
@@ -64,27 +23,26 @@ const Contact = () => (
             Don’t fill this out: <input name="bot-field" />
           </label>
         </p>
-        <p>
-          <label>
-            Your name:<br />
-            <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your email:<br />
-            <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:<br />
-            <textarea name="message" />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input className="form-control" type="text" name="name" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input className="form-control" type="email" name="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            className="form-control"
+            rows="10"
+            name="message"
+            required
+          />
+        </div>
+        <button className="btn-outline btn-md" type="submit">
+          SUBMIT
+        </button>
       </form>
     </section>
   </div>
