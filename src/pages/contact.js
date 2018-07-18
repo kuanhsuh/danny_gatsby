244c686d9329a0ebd3584b23580ee62d8a538691
dannyhuang <kuanhsuh@gmail.com>
2018-07-18 13:16:41 +0800

    retry

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,15 +20,33 @@ const Contact = () => (
         <input type="hidden" name="bot-field" />
         <div className="form-group">
           <label htmlFor="name">Name</label>
-          <input type="text" className="form-control" id="name" required />
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email</label>
-          <input type="email" className="form-control" id="email" required />
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="message">Message</label>
-          <textarea className="form-control" id="message" rows="10" required />
+          <textarea
+            className="form-control"
+            id="message"
+            rows="10"
+            name="message"
+            required
+          />
         </div>
         <button className="btn-outline btn-md" type="submit">
           SUBMIT
