70f76aee3edf59fb178db58e6f4ba2295d9ecbc9
dannyhuang <kuanhsuh@gmail.com>
2018-07-19 15:34:09 +0800

    edit card component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,14 +1,10 @@
 import React from "react";
 import "./card.scss";
 
-const Card = ({ title, description, link, linkTxt }) => (
+const Card = ({ imgSrc, imgAlt, title, description, link, linkTxt }) => (
   <div className="card">
     <div className="img-container">
-      <img
-        src="http://via.placeholder.com/550x350"
-        className="img-responsive"
-        alt=""
-      />
+      <img src={imgSrc} className="img-responsive" alt={imgAlt} />
     </div>
     <div className="card__description">
       <h3 className="mb10">{title}</h3>
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -9,6 +9,8 @@ const Portfolio = () => (
     <Header text="portfolio" type="mojito" />
     <div className="portfolio">
       <Card
+        imgSrc="http://via.placeholder.com/550x350"
+        imgAlt="Image Text"
         title="Sample Title one"
         description="this is a sample description"
         link="https://www.google.com"
