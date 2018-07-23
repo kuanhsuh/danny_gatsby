37200ee0627d0ef9a145426e669c3ef392ce894d
dannyhuang <kuanhsuh@gmail.com>
2018-07-23 18:53:02 +0800

    portfolio json file

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Header from "components/header/header";
 import Card from "components/card/card";
-
-const aux = props => props.children;
+import portfolios from "./portfolios.json";
 
 const Portfolio = () => (
   <aux>
     <Header text="portfolio" type="mojito" />
     <div className="portfolio">
-      <Card
-        imgSrc="http://via.placeholder.com/550x350"
-        imgAlt="Image Text"
-        title="Sample Title one"
-        description="this is a sample description"
-        link="https://www.google.com"
-        linkTxt="Github"
-      />
+      {portfolios.map(portfolio => (
+        <Card
+          imgSrc={portfolio.imgSrc}
+          imgAlt={portfolio.imgAlt}
+          title={portfolio.title}
+          description={portfolio.description}
+          link={portfolio.link}
+          linkTxt={portfolio.linkTxt}
+        />
+      ))}
     </div>
   </aux>
 );
