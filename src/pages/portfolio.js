e74639462ac6647a440c3bc725a3df00057c7b19
dannyhuang <kuanhsuh@gmail.com>
2018-07-24 14:00:00 +0800

    fix images and card

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,21 +1,24 @@
 import React from "react";
 import Header from "components/header/header";
 import Card from "components/card/card";
-import portfolios from "./portfolios.json";
+import { portfolios } from "./portfolios.js";
 
 const Portfolio = () => (
   <aux>
     <Header text="portfolio" type="mojito" />
     <div className="portfolio">
-      {portfolios.map(portfolio => (
-        <Card
-          imgSrc={portfolio.imgSrc}
-          title={portfolio.title}
-          description={portfolio.description}
-          link={portfolio.link}
-          linkTxt={portfolio.linkTxt}
-        />
-      ))}
+      {portfolios.map(portfolio => {
+        console.log(portfolio.imgSrc);
+        return (
+          <Card
+            imgSrc={portfolio.imgSrc}
+            title={portfolio.title}
+            description={portfolio.description}
+            link={portfolio.link}
+            linkTxt={portfolio.linkTxt}
+          />
+        );
+      })}
     </div>
   </aux>
 );
