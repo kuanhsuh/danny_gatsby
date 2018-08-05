3b326691b5fdb4438f29665bf8fd306273f3e727
dannyhuang <kuanhsuh@gmail.com>
2018-08-05 20:54:58 +0800

    Fix portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,28 +1,36 @@
 import React from "react";
 import Header from "components/header/header";
-// import Card from "components/card/card";
-// import { portfolios } from "./portfolios.js";
+import Card from "components/card/card";
+
+import pembridge from "assets/pembridge.png";
+import netflix from "assets/netflix.png";
+import barsumo from "assets/barsumo.png";
 
 const Portfolio = () => (
   <div>
     <Header text="portfolio" type="mojito" />
     <div className="portfolio">
-      <div className="card">
-        <div className="img-container">
-          <img
-            src="http://via.placeholder.com/350x150"
-            className="img-responsive"
-            alt="placeholder"
-          />
-        </div>
-        <div className="card__description">
-          <h3 className="mb10">title</h3>
-          <p className="mb-auto">description</p>
-          <a href="#" className="btn-outline">
-            Github
-          </a>
-        </div>
-      </div>
+      <Card
+        imgSrc={pembridge}
+        title="Pembridge Furniture Store"
+        description="Pembridge is a local furniture store. I built a custom wordpress website to help them gain online presence."
+        link="http://pembridge.com.tw/"
+        linkTxt="Link"
+      />
+      <Card
+        imgSrc={netflix}
+        title="Netflix Clone"
+        description="This is my side project where I built a netflix-clone. This app has upcoming movie, auto complete, and movie details."
+        link="https://github.com/kuanhsuh/netflix-clonev2"
+        linkTxt="Github"
+      />
+      <Card
+        imgSrc={barsumo}
+        title="Barsumo App"
+        description="Barsumo is an ordering platform that simplifies supplier ordering for Bars and Restaurants. I built a feature for them with graphQL."
+        link="https://www.barsumo.com/"
+        linkTxt="Link"
+      />
     </div>
   </div>
 );
